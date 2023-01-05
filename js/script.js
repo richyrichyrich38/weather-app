4b7e79461f40c86ed56ef563ab4f95a1e2bd4574
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2023-01-05 13:53:23 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,3 +1,51 @@
+
+
+// Wait for the document to be ready
+$(document).ready(function() {
+  // Listen for the form submission
+  $('#search-form').submit(function(event) {
+    // Prevent the form from actually submitting
+    event.preventDefault();
+    
+    // Get the value of the city input field
+    var city = $('#search-input').val();
+    
+    // Make an AJAX request to the weather API
+    $.ajax({
+      url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=699d121f2d0442551ebd49fa0b9d2e65',
+      dataType: 'json',
+      success: function(data) {
+        // Extract the relevant data from the API response
+        var temperature = data.main.temp;
+        var humidity = data.main.humidity;
+        var forecast = data.weather[0].description;
+        
+        // Update the weather data div with the new data
+        $('#forecast').html('Temperature: ' + temperature + '<br>' +
+                                'Humidity: ' + humidity + '<br>' +
+                                'Forecast: ' + forecast);
+      }
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // city data
 //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
@@ -7,34 +55,34 @@
 //api key
 //699d121f2d0442551ebd49fa0b9d2e65
 
-var apiKey = '699d121f2d0442551ebd49fa0b9d2e65';
-var city = 'London';
-var baseURL = 'https://api.openweathermap.org/data/2.5/'
-var currentURL = baseURL + `weather?appid=${apiKey}&units=metric&`;
-var forecastURL = baseURL + `weather?appid=${apiKey}&units=metric&`;
+// var apiKey = '699d121f2d0442551ebd49fa0b9d2e65';
+// var city = 'London';
+// var baseURL = 'https://api.openweathermap.org/data/2.5/'
+// var currentURL = baseURL + `weather?appid=${apiKey}&units=metric&`;
+// var forecastURL = baseURL + `weather?appid=${apiKey}&units=metric&`;
 
 
-function inputSubmitted(cityName) {
-  $.get(currentURL + `q=${cityName}`)
-    .then(function(currentData) {
-      console.log(currentData);
+// function inputSubmitted(cityName) {
+//   $.get(currentURL + `q=${cityName}`)
+//     .then(function(currentData) {
+//       console.log(currentData);
 
-      console.log(`
-      Temp: ${Math.round(currentData.main.temp)}
-      Humidity: ${currentData.main.humidity}%
-      Wind Speed: ${currentData.wind.speed}
-      `);
+//       console.log(`
+//       Temp: ${Math.round(currentData.main.temp)}
+//       Humidity: ${currentData.main.humidity}%
+//       Wind Speed: ${currentData.wind.speed}
+//       `);
 
-      $.get(forecastURL + `lat=${currentData.coord.lat}&lon=${currentData.coord.lon}`)
-        .then(function(forecastData) {
-          console.log(forecastData);
+//       $.get(forecastURL + `lat=${currentData.coord.lat}&lon=${currentData.coord.lon}`)
+//         .then(function(forecastData) {
+//           console.log(forecastData);
 
-        })
+//         })
 
-    })
-}
+//     })
+// }
 
-inputSubmitted(city)
+// inputSubmitted(city)
 
 
 // var searchInput = document.querySelector('#search-input');
